cac6bcfbbdb7d5ac563cc589589719f90dd6bcd0
Jimmy Shen <shenzui_da@hotmail.com>
2017-10-24 14:59:50 +0800

    feat: display order list

--- src/main/java/com/jimmmy/sell/controller/BuyerOrderController.java
+++ src/main/java/com/jimmmy/sell/controller/BuyerOrderController.java
@@ -10,14 +10,16 @@ import com.jimmmy.sell.service.OrderService;
 import com.jimmmy.sell.utils.ResultVOUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -53,8 +55,22 @@ public class BuyerOrderController {
     }
 
     // 订单列表
+    @GetMapping("/list")
+    public ResultVO<List<OrderDTO>> list(@RequestParam("openid") String openid,
+                                         @RequestParam(value = "page", defaultValue = "0") Integer page,
+                                         @RequestParam(value = "size", defaultValue = "10") Integer size) {
+        if (StringUtils.isEmpty(openid)) {
+            log.error("[查询订单列表] openid为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
 
+        PageRequest request = new PageRequest(page, size);
+        Page<OrderDTO> orderDTOPage = orderService.findList(openid, request);
+
+        return ResultVOUtil.success(orderDTOPage.getContent());
+    }
     // 订单详情
+
 
     // 取消订单
 }
--- src/main/java/com/jimmmy/sell/dto/OrderDTO.java
+++ src/main/java/com/jimmmy/sell/dto/OrderDTO.java
@@ -1,16 +1,21 @@
 package com.jimmmy.sell.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.jimmmy.sell.domain.OrderDetail;
 import com.jimmmy.sell.enums.OrderStatusEnum;
 import com.jimmmy.sell.enums.PayStatusEnum;
+import com.jimmmy.sell.utils.serializer.Date2LongSerializer;
 import lombok.Data;
 
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderDTO {
     /* 订单ID */
     private  String orderId;
@@ -37,10 +42,12 @@ public class OrderDTO {
     private Integer payStatus = PayStatusEnum.UNPAY.getCode();
 
     /* 创建时间 */
+    @JsonSerialize(using = Date2LongSerializer.class)
     private Date createTime;
 
     /* 更新时间 */
+    @JsonSerialize(using = Date2LongSerializer.class)
     private Date updateTime;
 
-    List<OrderDetail> orderDetailList;
+    List<OrderDetail> orderDetailList = new ArrayList<>();
 }
