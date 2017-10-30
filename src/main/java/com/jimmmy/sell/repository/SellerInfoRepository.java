c9f01798f72d3e6d09f3c570a7acbeceb6c9b948
Jimmy Shen <shenzui_da@hotmail.com>
2017-10-30 23:37:33 +0800

    ready for login/logout developing

---
+++ src/main/java/com/jimmmy/sell/repository/SellerInfoRepository.java
@@ -0,0 +1,9 @@
+package com.jimmmy.sell.repository;
+
+import com.jimmmy.sell.domain.SellerInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerInfoRepository extends JpaRepository<SellerInfo, String> {
+
+    SellerInfo findByOpenid(String openid);
+}
---
+++ src/test/java/com/jimmmy/sell/repository/SellerInfoRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.jimmmy.sell.repository;
+
+import com.jimmmy.sell.domain.SellerInfo;
+import com.jimmmy.sell.utils.KeyUtil;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SellerInfoRepositoryTest {
+
+    @Autowired
+    private SellerInfoRepository repository;
+
+    @Test
+    public void save() {
+        SellerInfo sellerInfo = new SellerInfo();
+        sellerInfo.setSellerId(KeyUtil.genUniqueKey());
+        sellerInfo.setUsername("admin");
+        sellerInfo.setPassword("admin");
+        sellerInfo.setOpenid("abc");
+
+        SellerInfo result = repository.save(sellerInfo);
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void findByOpenid() throws Exception {
+        SellerInfo result = repository.findByOpenid("abc");
+        Assert.assertEquals("abc", result.getOpenid());
+    }
+
+}
